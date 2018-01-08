edcaaa1c390a04a6997932bd24d20531dacdbe88
Kevin Yokley <kyokley2@gmail.com>
2018-01-07 22:49:37 -0600

    Adding test for ethereum pubkey

--- tests/test_account.py
+++ tests/test_account.py
@@ -97,3 +97,29 @@ class TestEthereumAccountGenerate(object):
         assert ret_val._pub_key == self.mock_generate.return_value.get_verifying_key.return_value
         self.mock_generate.assert_called_once_with(curve=SECP256k1)
         self.mock_generate.return_value.get_verifying_key.assert_called_once_with()
+
+class TestEthereumAccountPubKey(object):
+    def setup_method(self):
+        self.deserialize_patcher = mock.patch('papyrus.account.Wallet.deserialize')
+        self.mock_deserialize = self.deserialize_patcher.start()
+
+        self.priv_key = mock.MagicMock()
+        self.account = EthereumAccount(priv_key=self.priv_key)
+
+    def teardown_method(self):
+        self.deserialize_patcher.stop()
+
+    def test_no_pub_key(self):
+        expected = self.priv_key.get_verifying_key.return_value.to_string.return_value.hex.return_value
+        actual = self.account.pub_key()
+
+        assert expected == actual
+        self.priv_key.get_verifying_key.assert_called_once_with()
+
+    def test_pub_key(self):
+        self.account._pub_key = mock.MagicMock()
+        expected = self.account._pub_key.to_string.return_value.hex.return_value
+        actual = self.account.pub_key()
+
+        assert expected == actual
+        assert not self.priv_key.get_verifying_key.called
